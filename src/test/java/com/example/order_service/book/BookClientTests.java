ba4a41450e64e59bc220d85725db0514cc448b9a
devscope777 <devscope777@gmail.com>
2026-03-12 16:06:25 -0400

    Refactor BookClientTests class. Add domain tests

--- src/test/java/com/example/order_service/book/BookClientTests.java
+++ src/test/java/com/example/order_service/book/BookClientTests.java
@@ -2,8 +2,9 @@ package com.example.order_service.book;
 
 import java.io.IOException;
 
-import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,19 +15,31 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 public class BookClientTests {
+    private MockWebServer mockWebServer;
+    private BookClient bookClient;
+
+    @BeforeEach
+    public void setup() throws IOException {
+        this.mockWebServer = new MockWebServer();
+        this.mockWebServer.start();
+        var webClient = WebClient.builder()
+                .baseUrl(mockWebServer.url("/").uri().toString()).build();
+
+        this.bookClient = new BookClient(webClient);
+
+    }
+
+    @AfterEach
+    public void clean() throws IOException {
+        // Clean up
+        mockWebServer.shutdown();
+        mockWebServer.close();
+    }
 
     @Test
     public void whenBookExistsThenReturnBook() throws IOException {
         // Arrange
-        MockWebServer mockWebServer = new MockWebServer();
-        mockWebServer.start();
-        var webClient = WebClient.builder()
-                .baseUrl(mockWebServer.url("/").uri().toString()).build();
-
-        BookClient bookClient = new BookClient(webClient);
-
         var bookIsbn = "1234567890";
-
         var mockResponse = new MockResponse()
                 .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .setBody(String.format("""
@@ -47,8 +60,5 @@ public class BookClientTests {
         // Assert
         StepVerifier.create(book).expectNextMatches(b -> b.isbn().equals(bookIsbn)).verifyComplete();
 
-        // Clean up
-        mockWebServer.shutdown();
-        mockWebServer.close();
     }
 }
---
+++ src/test/java/com/example/order_service/domain/OrderRepositoryR2dbcTests.java
@@ -0,0 +1,53 @@
+package com.example.order_service.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.r2dbc.test.autoconfigure.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import com.example.order_service.persistence.DataConfig;
+import com.example.order_service.service.OrderService;
+
+import reactor.test.StepVerifier;
+
+@DataR2dbcTest
+@Import(DataConfig.class)
+@Testcontainers
+public class OrderRepositoryR2dbcTests {
+
+    @Container
+    public static PostgreSQLContainer postgresql = new PostgreSQLContainer(DockerImageName.parse("postgres:13.4"));
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @DynamicPropertySource
+    public static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", OrderRepositoryR2dbcTests::r2dbcUrl);
+        registry.add("spring.r2dbc.username", postgresql::getUsername);
+        registry.add("spring.r2dbc.password", postgresql::getPassword);
+        registry.add("spring.flyway.url", postgresql::getJdbcUrl);
+    }
+
+    private static String r2dbcUrl() {
+        return String.format("r2dbc:postgresql://%s:%s/%s",
+                postgresql.getHost(),
+                postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
+                postgresql.getDatabaseName());
+    }
+
+    @Test
+    public void createRejectedOrder() {
+        var rejectedOrder = OrderService.buildRejectedOrder("1234567890", 3);
+        StepVerifier.create(orderRepository.save(rejectedOrder))
+                .expectNextMatches(order -> order.status().equals(OrderStatus.REJECTED))
+                .verifyComplete();
+    }
+}
