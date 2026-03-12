b92b3f81233eb70047b9f959c50bc9f97182467a
devscope777 <devscope777@gmail.com>
2026-03-12 14:12:20 -0400

    Add test for when book exists

--- src/test/java/com/example/order_service/book/BookClientTests.java
+++ src/test/java/com/example/order_service/book/BookClientTests.java
@@ -1,5 +1,54 @@
 package com.example.order_service.book;
 
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
 public class BookClientTests {
 
+    @Test
+    public void whenBookExistsThenReturnBook() throws IOException {
+        // Arrange
+        MockWebServer mockWebServer = new MockWebServer();
+        mockWebServer.start();
+        var webClient = WebClient.builder()
+                .baseUrl(mockWebServer.url("/").uri().toString()).build();
+
+        BookClient bookClient = new BookClient(webClient);
+
+        var bookIsbn = "1234567890";
+
+        var mockResponse = new MockResponse()
+                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .setBody(String.format("""
+                        {
+                        "isbn": %s,
+                        "title": "Title",
+                        "author": "Author",
+                        "price": 9.90,
+                        "publisher": "Polarsophia"
+                        }
+                        """, bookIsbn));
+
+        mockWebServer.enqueue(mockResponse);
+
+        // Act
+        Mono<Book> book = bookClient.getBookByIsbn(bookIsbn);
+
+        // Assert
+        StepVerifier.create(book).expectNextMatches(b -> b.isbn().equals(bookIsbn)).verifyComplete();
+
+        // Clean up
+        mockWebServer.shutdown();
+        mockWebServer.close();
+    }
 }
